e827dfbd33e4a474bef32bed5d657853c895e534
Demetryy <61795534+Demetryy@users.noreply.github.com>
2021-07-01 21:28:13 +0300

    Update DataBase.cs

--- ClassLibrary1/DataBase.cs
+++ ClassLibrary1/DataBase.cs
@@ -1,26 +1,41 @@
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using MySql.Data;
-using MySqlConnector;
 using MySql.Data.MySqlClient;
 
 namespace ClassLibrary1
 {
     public class DataBase
     {
-        public void DataBaseConnect()
+        List<User> _user;
+        List<Account> _account;
+        List<Role> _role;
+        List<AccountRole> _accountRole;
+        public static MySqlConnection DataBaseConnect(string host, int port, string database, string username, string password)
         {
-            
+            String connString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + password;
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
         }
 
-        public void DataBaseExport()
+        public static void DataBaseExport(MySqlConnection connect, string table)
         {
-            
+            string queryString =
+                $"SELECT * FROM " + table;  
+            //MySqlDataAdapter adapter = new MySqlDataAdapter(queryString, connect);
         }
 
-        public void DataBaseImport()
+        public static List<string> DataBaseImport(MySqlConnection connect, string table)
         {
-            
+            var  command = connect.CreateCommand();
+            command.CommandText = $"SELECT * FROM " + table;
+            connect.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            var dataList = new List<string>();
+            /*
+             ...
+             */
+            return dataList;
         }
     }
 }
