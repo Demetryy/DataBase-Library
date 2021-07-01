d917ab40aa9fa1a979ec71598680d5069c56bc29
Demetryy <61795534+Demetryy@users.noreply.github.com>
2021-07-01 21:28:40 +0300

    Update User.cs

--- ClassLibrary1/User.cs
+++ ClassLibrary1/User.cs
@@ -5,5 +5,15 @@ namespace ClassLibrary1
         public string Name { get; set; }
         public string SurName { get; set; }
         public string Contacts { get; set; }
+        
+        public User(string name, string surname, string contacts)
+        {
+            Name = name;
+            SurName = surname;
+            Contacts = contacts;
+        }
+        
+        public User(){}
     }
+
 }
